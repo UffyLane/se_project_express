d0cead1708fd38f9d87ff104f76f5e7ab1ecc106
Stuart G Clark Jr. <darose07@yahoo.com>
2026-01-19 07:43:07 +0000

    Add validation middleware to auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+const { validateLogin, validateCreateUser } = require('../middlewares/validation');
+
 const { login, createUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
@@ -7,8 +9,8 @@ const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 
 // PUBLIC AUTH ROUTES
-router.post("/signin", login);
-router.post("/signup", createUser);
+router.post('/signin', validateLogin, login);
+router.post('/signup', validateCreateUser, createUser);
 
 // PROTECTED ROUTES
 router.use("/users", auth, userRouter);
