6b09cfe2054eef748b856f38b75d67e3c1b7dbc7
Stuart G Clark Jr. <darose07@yahoo.com>
2026-01-19 07:32:40 +0000

    Consolidate clothing item routes into single file

--- routes/clothingItemsPublic.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getClothingItems } = require('../controllers/clothingItems');
-
-// Public GET all items
-router.get('/', getClothingItems);
-
-module.exports = router;
